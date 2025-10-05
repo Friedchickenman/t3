f7b597718ad76f638ea30a2ec3dc8f1e6bf18a8c
SungJun <psj12343@gmail.com>
2025-10-05 23:47:18 +0900

    10/5 12th commit

---
+++ src/components/todo/todoAdd.tsx
@@ -0,0 +1,32 @@
+import {useState} from "react";
+
+const initState:Todo = {
+    title: 'Title',
+    writer: 'writer'
+}
+
+function TodoAdd() {
+
+    const [todoAdd, setTodoAdd] = useState(initState)
+
+    return (
+        <div>
+            <h1> Todo Add </h1>
+
+            <div>
+                TITLE
+                <input type='text' value={todoAdd.title}></input>
+            </div>
+
+            <div>
+                WRITER
+                <input type='text' value={todoAdd.writer}></input>
+            </div>
+            <div>
+            <button >SEND</button>
+            </div>
+        </div>
+    );
+}
+
+export default TodoAdd;
